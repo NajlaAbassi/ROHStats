fb71689efea9dc4d8b83a1a43106554502665ad5
Najla Abassi <abassi.nejla96@gmail.com>
2024-12-26 00:44:31 +0100

    add example to roh_stat

--- R/roh_stat.R
+++ R/roh_stat.R
@@ -7,17 +7,28 @@
 #' @export
 #'
 #' @examples
-#' 
+#' # Create a toy dataset
+#' toy_data <- data.frame(
+#'   KB = c(1500, 2500, 4000, 1000, 3000),
+#'   NSEG = c(2, 4, 6, 1, 3)
+#' )
+#' # Write the dataset to a temporary file
+#' toy_file <- tempfile(fileext = ".txt")
+#' write.table(toy_data, file = toy_file, sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
+#'
+#' # Run roh_stat()
+#' roh_stat(toy_file)
+#'
 roh_stat <- function(indiv_path) {
   dataset <- read.table(indiv_path, sep = "", header = T)
-  # total length of ROH per individual
-  #Maximum
+  # Total length of ROH per individual
+  # Maximum
   maximum <- round(max(dataset$KB, na.rm = TRUE)/1000,2)
-  #Minimum
+  # Minimum
   minimum <- round(min(dataset$KB, na.rm = TRUE)/1000,2)
-  #Mean
+  # Mean
   mean_value <- round(mean(dataset$KB, na.rm = TRUE)/1000,2)
-  #Standard Deviation
+  # Standard Deviation
   sd_value <- round(sd(dataset$KB, na.rm = TRUE)/1000,2)
   # Summary table
   sum_table_1 <- as.data.frame(cbind(maximum,minimum,mean_value,sd_value))
@@ -25,21 +36,21 @@ roh_stat <- function(indiv_path) {
   write.table(sum_table_1,"total_length_statistics.txt", sep = "\t",
               col.names = T,row.names = F,quote = F)
   summary_table_1 <- read.table("total_length_statistics.txt",header = T)
-  ######## number of ROH per individual##########
-  #Maximum
+  # Number of ROH per individual
+  # Maximum
   maximum <- round(max(dataset$NSEG, na.rm = TRUE),2)
-  #Minimum
+  # Minimum
   minimum <- round(min(dataset$NSEG, na.rm = TRUE),2)
-  #Mean
+  # Mean
   mean_value <- round(mean(dataset$NSEG, na.rm = TRUE),2)
-  #Standard Deviation
+  # Standard Deviation
   sd_value <- round(sd(dataset$NSEG, na.rm = TRUE),2)
   # Summary table
   sum_table_2 <- as.data.frame(cbind(maximum,minimum,mean_value,sd_value))
   rownames(sum_table_2)[1] <- "total number"
-  write.table(sum_table_2,"mean_total_length_statistics.txt", sep = "\t",
+  write.table(sum_table_2,"mean_total_number_statistics.txt", sep = "\t",
               col.names = T,row.names = F,quote = F)
-  summary_table_2 <- read.table("mean_total_length_statistics.txt",header = T)
+  summary_table_2 <- read.table("mean_total_number_statistics.txt",header = T)
   common_col_names <- intersect(names(summary_table_1), names(summary_table_2))
   # Summary table
   summary_table <- merge(summary_table_1, summary_table_2,
