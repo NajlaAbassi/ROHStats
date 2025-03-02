7008caefe7d4c2ff7726415291c89433aedc8e8d
NajlaAbassi <abassi.nejla96@gmail.com>
2025-03-02 16:58:55 +0100

    update function to be flexible for different chromosome counts and avoid using for loops :v

--- R/homozyg_density.R
+++ R/homozyg_density.R
@@ -13,15 +13,12 @@
 homozyg_density <- function(bim_path) {
   bim_file <- read.table(bim_path, sep = "\t", header = FALSE)
   snp_nb <- nrow(bim_file)
-  covered_genome_size = 0
-  for (i in 1:22) {
-    dataset_chr_i <- bim_file[ bim_file$V1 == i, ]
-    size_chr_i <- max(dataset_chr_i$V4, na.rm = FALSE) - min(dataset_chr_i$V4,
-                                                             na.rm = FALSE)
-    covered_genome_size <- covered_genome_size + size_chr_i
-  }
-  couverage <- covered_genome_size
-  density <- snp_nb/couverage * 1000
+  chromosomes <- unique(bim_file$V1)
+  covered_genome_size <- sum(sapply(chromosomes, function(chr) {
+    dataset_chr_i <- bim_file[bim_file$V1 == chr, ]
+    return(max(dataset_chr_i$V4, na.rm = TRUE, -Inf) - min(dataset_chr_i$V4, na.rm = TRUE, Inf))
+  }), na.rm = TRUE)
+  density <- snp_nb/covered_genome_size  * 1000
   snp_density <- 1 / density
   return(snp_density)
 }
