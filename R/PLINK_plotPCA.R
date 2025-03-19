bf5cd3a6cbce308b37d85afe3dcf5b461cf5ea54
Najla Abassi <abassi.nejla96@gmail.com>
2025-03-19 12:33:35 +0100

    updating the pca function

--- R/PLINK_plotPCA.R
+++ R/PLINK_plotPCA.R
@@ -1,43 +1,42 @@
-#' PLINK_pca
-#' PLINK_pca() generates a PCA plot of the populations from a PLINK bed file
+#' PLINK_plotPCA
+#' PLINK_plotPCA() generates a PCA plot of the populations from a PLINK bed file
 #'
 #' @param bed_file  A character string indicating the path of the .bed file
 #' @param pop A character string indicating the path of a text file with the
 #' population names (identical to the first column in a .fam file)
 #'
-#' @return pca plot
+#' @return pca_plot: ggplot object representing the PCA plot
 #' @export
-#' @importFrom BEDMatrix BEDMatrix 
+#' @importFrom utils read.table
+#' @importFrom BEDMatrix BEDMatrix
 #' @importFrom FactoMineR PCA
 #' @importFrom factoextra fviz_eig
 #' @importFrom factoextra fviz_pca_ind
 #' @importFrom ggplot2 ggsave
 #'
 #' @examples
-#' 
-PLINK_pca <- function(bed_file, pop){
+#' \dontrun{
+#' PLINK_pca("data/genotype.bed","data/population.txt")
+#' }
+#'
+PLINK_plotPCA <- function(bed_file,
+                      pop){
   # load SNP data using BEDMatrix
   snp_data <- BEDMatrix(bed_file)
-  
+
   # read family data
-  family <- read.table(fam_file)
-  
+  family <- read.table(pop)
+
   # convert SNP data to a matrix and combine with family data
   snp_matrix <- as.matrix(snp_data)
   combined_data <- cbind(family, snp_matrix)
-  
+
   # perform PCA
   pca <- PCA(snp_matrix, graph = FALSE)
-  
-  # plot eigenvalues
-  fviz_eig(pca)
-  
+
   # plot individuals with their respective family labels
-  pca <- fviz_pca_ind(pca, habillage = combined_data$V1, repel = TRUE)
-  
-  # save plot
-  ggsave("figures/pca.png", plot = pca, create.dir = TRUE)
-  
-  return(pca)
-} 
+  pca_plot <- fviz_pca_ind(pca, habillage = combined_data$V1, repel = TRUE)
+
+  return(pca_plot)
+}
 
