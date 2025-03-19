76181a5abc9c36a758686f7acbf2e9fc80efe4b1
Najla Abassi <abassi.nejla96@gmail.com>
2025-03-19 12:32:34 +0100

    updating dependencies

--- R/F_hom.R
+++ R/F_hom.R
@@ -7,31 +7,13 @@
 #'  \item{Fhom_summary_table}{A data frame with overall Fhom summary statistics (Maximum, Minimum, Mean, SD)}
 #'  \item{by_population_Fhom_summary}{A data frame with per population Fhom summary statistics (Maximum, Minimum, Mean, SD)}
 #' @export
-#' 
-#' @import dplyr
+#'
+#' @importFrom dplyr  %>%
+#' @importFrom utils read.table
+#' @importFrom stats sd
 #'
 #' @examples
-#' # simulating a sample PLINK .het dataset
-#' sample_data <- data.frame(
-#'   FID = c("FAM001", "FAM002", "FAM003"),
-#'   IID = c("ID001", "ID002", "ID003"),
-#'   O_HOM = c(50, 30, 40),
-#'   E_HOM = c(45, 32, 38),
-#'   N_NM = c(100, 100, 100),
-#'   F = c(0.05, -0.02, 0.08)
-#' )
 #'
-#' # save sample data to a temporary file?
-#' temp_file <- tempfile(fileext = ".het")
-#' write.table(sample_data, temp_file, sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
-#'
-#' # run F_hom on the simulated data
-#' result <- F_hom(temp_file)
-#'
-#' # access results
-#' result$FHOM_values
-#' result$summary_table
-#' 
 F_hom <- function(het_path) {
   # read input
   dataset <- read.table(het_path, header = T)
@@ -46,14 +28,14 @@ F_hom <- function(het_path) {
   mean_value <- round(mean(FHOM$Fhom, na.rm = TRUE),2)
   # standard deviation
   sd_value <- round(sd(FHOM$Fhom, na.rm = TRUE),2)
-  
+
   Fhom_summary_table <- data.frame(
     Maximum = maximum,
     Minimum = minimum,
     Mean = mean_value,
     SD = sd_value
   )
-  
+
   # compute statistics by population
   by_population_Fhom_summary <- FHOM %>%
     group_by(FID) %>%
@@ -65,8 +47,8 @@ F_hom <- function(het_path) {
       .groups = 'drop'
     ) %>%
     as.data.frame()
-  
-  
+
+
   # return output as a list
   return(list(FHOM_values = FHOM,
               Fhom_summary_table = Fhom_summary_table,
